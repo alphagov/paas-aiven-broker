5b55325762d76866266801f86cdc597a826bd3d1
henrytk <henrytknott@gmail.com>
2017-12-10 16:41:08 +0000

    Refactor config tests
    This makes it clearer which config each test is running with.

--- broker/config_test.go
+++ broker/config_test.go
@@ -11,104 +11,91 @@ import (
 )
 
 var _ = Describe("Config", func() {
-	var (
-		err               error
-		config            Config
-		validConfigSource = `
+
+	var configSource string
+
+	It("requires a basic auth username", func() {
+		configSource = `
 			{
-				"basic_auth_username":"admin",
 				"basic_auth_password":"1234",
 				"port": "8080",
 				"log_level": "debug",
-				"catalog": {
-					"services": [
-						{
-							"id": "1",
-							"provider_config": {
-								"provider_specific_service_config": "blah"
-							},
-							"plans": [
-								{
-									"id": "1",
-									"provider_specific_plan_config": "blah"
-								}
-							]
-						}
-					]
-				}
+				"catalog": {}
 			}
 		`
-	)
-
-	BeforeEach(func() {
-		config, err = NewConfig(strings.NewReader(validConfigSource))
+		config, err := NewConfig(strings.NewReader(configSource))
 		Expect(err).NotTo(HaveOccurred())
-	})
-
-	It("requires a basic auth username", func() {
-		config.API.BasicAuthUsername = ""
 
 		err = config.Validate()
 		Expect(err).To(MatchError("Config error: basic auth username required"))
 	})
 
 	It("requires a basic auth password", func() {
-		config.API.BasicAuthPassword = ""
+		configSource = `
+			{
+				"basic_auth_username":"username",
+				"port": "8080",
+				"log_level": "debug",
+				"catalog": {}
+			}
+		`
+		config, err := NewConfig(strings.NewReader(configSource))
+		Expect(err).NotTo(HaveOccurred())
 
 		err = config.Validate()
 		Expect(err).To(MatchError("Config error: basic auth password required"))
 	})
 
 	It("helps convert log level string values into lager.LogLevel values", func() {
+		configSource = `
+			{
+				"basic_auth_username":"username",
+				"basic_auth_password":"1234",
+				"port": "8080",
+				"log_level": "debug",
+				"catalog": {}
+			}
+		`
+		config, err := NewConfig(strings.NewReader(configSource))
+		Expect(err).NotTo(HaveOccurred())
+
 		lagerLogLevel := config.API.LagerLogLevel()
 		Expect(lagerLogLevel).To(Equal(lager.DEBUG))
 	})
 
 	Describe("default values", func() {
-		var (
-			configMissingDefaults = `
+		It("sets a default port", func() {
+			configSource = `
 				{
-					"basic_auth_username":"admin",
+					"basic_auth_username":"username",
 					"basic_auth_password":"1234",
-					"catalog": {
-						"services": [
-							{
-								"id": "1",
-								"provider_config": {
-									"provider_specific_service_config": "blah"
-								},
-								"plans": [
-									{
-										"id": "1",
-										"provider_specific_plan_config": "blah"
-									}
-								]
-							}
-						]
-					}
+					"log_level": "debug",
+					"catalog": {}
 				}
 			`
-		)
-
-		BeforeEach(func() {
-			config, err = NewConfig(strings.NewReader(configMissingDefaults))
+			config, err := NewConfig(strings.NewReader(configSource))
 			Expect(err).NotTo(HaveOccurred())
+
+			err = config.Validate()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(config.API.Port).To(Equal(DefaultPort))
 		})
 
-		Context("when missing port", func() {
-			It("sets a default value", func() {
-				err = config.Validate()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(config.API.Port).To(Equal(DefaultPort))
-			})
-		})
+		It("sets a default log_level", func() {
+			configSource = `
+				{
+					"basic_auth_username":"username",
+					"basic_auth_password":"1234",
+					"port": "8080",
+					"catalog": {}
+				}
+			`
+			config, err := NewConfig(strings.NewReader(configSource))
+			Expect(err).NotTo(HaveOccurred())
 
-		Context("when missing log_level", func() {
-			It("sets a default value", func() {
-				err = config.Validate()
-				Expect(err).NotTo(HaveOccurred())
-				Expect(config.API.LogLevel).To(Equal(DefaultLogLevel))
-			})
+			err = config.Validate()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(config.API.LogLevel).To(Equal(DefaultLogLevel))
 		})
 	})
 })
