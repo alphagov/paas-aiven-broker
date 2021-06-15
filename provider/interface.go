dd8c2a3a92d16044718938da6186d02ee77cea5c
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2021-06-15 15:05:16 +0100

    update interface

--- provider/interface.go
+++ provider/interface.go
@@ -2,15 +2,14 @@ package provider
 
 import (
 	"context"
-
-	"github.com/pivotal-cf/brokerapi"
+	"github.com/pivotal-cf/brokerapi/domain"
 )
 
 type ServiceProvider interface {
-	Provision(context.Context, ProvisionData) (dashboardURL, operationData string, err error)
+	Provision(context.Context, ProvisionData, domain.ProvisionDetails) (dashboardURL, operationData string, err error)
 	Deprovision(context.Context, DeprovisionData) (operationData string, err error)
-	Bind(context.Context, BindData) (binding brokerapi.Binding, err error)
+	Bind(context.Context, BindData) (binding domain.Binding, err error)
 	Unbind(context.Context, UnbindData) (err error)
-	Update(context.Context, UpdateData) (operationData string, err error)
-	LastOperation(context.Context, LastOperationData) (state brokerapi.LastOperationState, description string, err error)
+	Update(context.Context, UpdateData, domain.UpdateDetails) (operationData string, err error)
+	LastOperation(context.Context, LastOperationData) (state domain.LastOperationState, description string, err error)
 }
